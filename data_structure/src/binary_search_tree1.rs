b9b41d09023348279b50b9f0c5b7cbe22d73eb2a
destinyfvcker <destinyfunk@163.com>
2024-09-25 20:54:51 +0800

    add another bst impl

---
+++ data_structure/src/binary_search_tree1.rs
@@ -0,0 +1,152 @@
+/*
+    binary_search tree
+    This problem requires you to implement a basic interface for a binary tree
+*/
+
+use std::cmp::Ordering;
+use std::fmt::Debug;
+
+#[derive(Debug)]
+struct TreeNode<T>
+where
+    T: Ord,
+{
+    value: T,
+    left: Option<Box<TreeNode<T>>>,
+    right: Option<Box<TreeNode<T>>>,
+}
+
+#[derive(Debug)]
+struct BinarySearchTree<T>
+where
+    T: Ord,
+{
+    root: Option<Box<TreeNode<T>>>,
+}
+
+impl<T> TreeNode<T>
+where
+    T: Ord,
+{
+    fn new(value: T) -> Self {
+        TreeNode {
+            value,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+impl<T> BinarySearchTree<T>
+where
+    T: Ord,
+{
+    fn new() -> Self {
+        BinarySearchTree { root: None }
+    }
+
+    // Insert a value into the BST
+    fn insert(&mut self, value: T) {
+        match self.root {
+            Some(ref mut node) => {
+                node.insert(value);
+            }
+            None => self.root = Some(Box::new(TreeNode::new(value))),
+        }
+    }
+
+    // Search for a value in the BST
+    fn search(&self, value: T) -> bool {
+        match self.root {
+            Some(ref root) => root.search(value),
+            None => false,
+        }
+    }
+}
+
+impl<T> TreeNode<T>
+where
+    T: Ord,
+{
+    // Insert a node into the tree
+    fn insert(&mut self, value: T) {
+        let mut target_child;
+        if value > self.value {
+            target_child = &mut self.right;
+        } else if value < self.value {
+            target_child = &mut self.left;
+        } else {
+            return;
+        }
+
+        match target_child {
+            Some(child) => {
+                child.insert(value);
+            }
+            None => {
+                *target_child = Some(Box::new(TreeNode::new(value)));
+            }
+        }
+    }
+
+    fn search(&self, value: T) -> bool {
+        let target_child;
+        if value > self.value {
+            target_child = &self.right;
+        } else if value < self.value {
+            target_child = &self.left;
+        } else {
+            return true;
+        }
+
+        match target_child {
+            Some(child) => child.search(value),
+            None => false,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_insert_and_search() {
+        let mut bst = BinarySearchTree::new();
+
+        assert_eq!(bst.search(1), false);
+
+        bst.insert(5);
+        bst.insert(3);
+        bst.insert(7);
+        bst.insert(2);
+        bst.insert(4);
+
+        assert_eq!(bst.search(5), true);
+        assert_eq!(bst.search(3), true);
+        assert_eq!(bst.search(7), true);
+        assert_eq!(bst.search(2), true);
+        assert_eq!(bst.search(4), true);
+
+        assert_eq!(bst.search(1), false);
+        assert_eq!(bst.search(6), false);
+    }
+
+    #[test]
+    fn test_insert_duplicate() {
+        let mut bst = BinarySearchTree::new();
+
+        bst.insert(1);
+        bst.insert(1);
+
+        assert_eq!(bst.search(1), true);
+
+        match bst.root {
+            Some(ref node) => {
+                assert!(node.left.is_none());
+                assert!(node.right.is_none());
+            }
+            None => panic!("Root should not be None after insertion"),
+        }
+    }
+}
